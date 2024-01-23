83aaf07e662194b82210694c283ac4e7968c3c95
StraWberY <136798733+MiguelOHara2099@users.noreply.github.com>
2024-01-23 21:21:13 +0300

    callback and one more thing

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
 	from aiogram import executor
 	from handlers import dp
 	
-	executor.start_polling(dp, on_startup=on_startup, skip_updates=True)#skip_updates=True для того чтобы бот не отправлял кучу раз повторные сообщения
+	executor.start_polling(dp, on_startup=on_startup, skip_updates=True)#skip_updates=True With this thing messages don't send more times
--- handlers/users/user_buttons.py
+++ handlers/users/user_buttons.py
@@ -2,6 +2,7 @@
 from aiogram import types
 from loader import dp
 from keyboards import catalog_list
+from loader import bot
 
 @dp.message_handler(text='Контакты')
 async def contacts(message: types.Message):
@@ -14,3 +15,13 @@ async def cart(message: types.Message):
 @dp.message_handler(text='Каталог')
 async def catalog(message: types.Message):
     await message.answer(f'Каталог пуст!', reply_markup=catalog_list)
+    
+#callback_data in client_cb.py
+@dp.callback_query_handler()
+async def callback_query_keyboard(callback_query: types.CallbackQuery):
+    if callback_query.data == 'geely':
+        await bot.set_message(chat_id=callback_query.from_user.id, text='вы выбрали geely')
+    elif callback_query.data == 'chery':
+        await bot.set_message(chat_id=callback_query.from_user.id, text='вы выбрали geely')
+    elif callback_query.data == 'changan':
+        await bot.set_message(chat_id=callback_query.from_user.id, text='вы выбрали geely')
