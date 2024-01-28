6f08207c39b7d07beeb6de202161c00c2a596e83
StraWberY <136798733+MiguelOHara2099@users.noreply.github.com>
2024-01-28 19:51:45 +0300

    fix

--- loader.py
+++ loader.py
@@ -1,8 +1,8 @@
-from aiogram import Bot, enums
+from aiogram import Bot, Dispatcher
 # from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from data import config
 
 # storage = MemoryStorage()
 
-bot = Bot(token=config.BOT_TOKEN, parse_mode=enums.parse_mode.ParseMode.HTML)
-# dp = Dispatcher(bot, storage=storage)
+bot = Bot(token=config.BOT_TOKEN) #, parse_mode=enums.parse_mode.ParseMode.HTML)
+dp = Dispatcher(bot=bot) 
