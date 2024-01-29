d7858fefafb96e53b8ca6b54fb441aae321be058
StraWberY <136798733+MiguelOHara2099@users.noreply.github.com>
2024-01-29 19:23:38 +0300

    1

--- app.py
+++ app.py
@@ -9,6 +9,6 @@ async def on_startup(dp):
 
 if __name__ == "__main__":
 	from aiogram import executor
-	from handlers import dp
+	from loader import dp
 	
 	executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
